fed2fc3884f03a242dfd75a9d315e8e922e76f04
Lymei <lymeicontato@gmail.com>
2023-05-17 01:05:16 -0300

    feat: added html viewer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import settings
+import markdown
+import tkhtmlview
 import md_manager as mg
 import customtkinter as ctk
 from tkinter import messagebox
@@ -11,15 +13,38 @@ class App(ctk.CTk):
         self.title(f'{title}')
         self.resizable(False, False)
 
+        self.rowconfigure(0, weight=1, uniform='a')
+        self.columnconfigure((0, 1), weight=1, uniform='a')
+
         self.md_manager = mg.MdManager()
+
+        self.html = ''
+        self.html_view = tkhtmlview.HTMLScrolledText(master=self, padx=5, pady=5, state='disabled', cursor='arrow',
+                                                     html=self.html)
+        self.html_view.configure(foreground='#fff', background='#333')
+        self.html_view.grid(column=1, row=0, sticky='nswe')
+        self.html_view.fit_height()
+
+        self.md_manager.last_added.trace('w', lambda *args: self.update_html_view())
+
         self.md_manager.add_heading(1, 'My TiTle')
         self.md_manager.add_heading(2, 'My SUBTITLE')
         self.md_manager.add_heading(3, 'My TEST')
-        self.md_manager.add_paragraph('Teste Aury')
 
-        self.bind_all('<Return>', lambda event: self.generate_md())
+        # self.bind_all('<Return>', lambda event: self.generate_md())
+        self.bind_all('<Return>', lambda event: self.md_manager.add_paragraph('My TiTle'))
 
         self.mainloop()
+
+    def update_html_view(self):
+        self.html = ''
+
+        for element in self.md_manager.elements:
+            self.html += element.markdown
+
+        self.html = markdown.markdown(self.html)
+
+        self.html_view.set_html(self.html)
 
     def generate_md(self):
         self.md_manager.create_md()
